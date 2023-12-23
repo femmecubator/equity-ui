54487270bf01efefd4082406f9d91c10e595e55b
Carlo <ca.fernando415@gmail.com>
2023-12-22 22:16:29 -0500

    fixing gh action related to octokit version

--- .github/actions/octokit-script.js
+++
@@ -1,6 +0,0 @@
-const octokit = new (require('@octokit/action'))();
-await octokit.issues.removeLabels({
-  owner: context.repo.owner,
-  repo: context.repo.repo,
-  issue_number: context.issue.number,
-});
