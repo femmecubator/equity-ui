c7c87be896105c0c4895275ff985813b09da89cf
Anh <vuonga1103@yahoo.com>
2024-01-24 21:27:26 -0500

    Wrap test components around theme provider

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,10 +1,11 @@
 import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { Button } from '.';
+import contextRender from '../../shared/test/contextRender';
 
 describe('Button component', () => {
   it('Button should render correctly', () => {
-    render(<Button>Hello!</Button>);
+    contextRender(<Button>Hello!</Button>);
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
   });
