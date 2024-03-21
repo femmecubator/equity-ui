5204bffcf259028cf9b13acdd4f808f900d2115a
Anh <vuonga1103@yahoo.com>
2024-03-21 18:46:16 -0400

    Add storybook scaffold for SectionTitle

---
+++ src/components/SectionTitle/__docs__/SectionTitle.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import type { Meta } from '@storybook/react';
+
+import SectionTitle from '../SectionTitle';
+
+const meta: Meta<typeof SectionTitle> = {
+  title: 'SectionTitle',
+  component: SectionTitle,
+  // argTypes: {
+  //   variant: {
+  //     options: ['primary', 'secondary'],
+  //     control: { type: 'radio' },
+  //   },
+  // },
+};
+
+export default meta;
+
+export const Basic = (args) => {
+  return <SectionTitle {...args} />;
+};
