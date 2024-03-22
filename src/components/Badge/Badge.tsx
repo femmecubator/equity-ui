881811d18e6b0ac6d8d45b350159a52b496a2623
Clae <56447997+clayruh@users.noreply.github.com>
2024-03-22 17:28:31 -0400

    CSS - Move CSS hardcoded border-radius style to semantic styling

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -2,17 +2,23 @@ import styled from '@emotion/styled';
 import { ReactNode } from 'react';
 
 // adding <BadgeProps> to tell it that the props are the same from Badge (ie. emphasis, text, etc)
+// CSS written in styled.div<BadgeProps>``</BadgeProps> is meant to be "default" hardcoded styles
 const StyledBadge = styled.div<BadgeProps>`
   display: inline-block;
   padding: 5px;
-  border-radius: 8px;
   background: #D4EFDF;
   ${({
     // add in styling
-
+    variant,
+    theme: {
+      // semantic: { border, spacing, color }
+      semantic: { border }
+    },
   }) =>
+  // all the following code comes from semantic.ts file because we said theme: { semantic: {base} }
+  // everything below here is meant to be dynamic
   `
-  
+    border-radius: ${border.radius.small};
   `}
 `;
 interface BadgeProps {
