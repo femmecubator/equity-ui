2829adb270205819defc0c2973bae3d77eac718c
Carlo <ca.fernando415@gmail.com>
2024-03-23 10:48:25 -0400

    saving progress on new approach

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,190 +1,129 @@
-import React, { ComponentPropsWithoutRef, ReactNode } from 'react';
+import { CSSProperties, ComponentPropsWithoutRef } from 'react';
 import styled, { CSSObject } from '@emotion/styled';
-import { css } from '@emotion/react';
+import { Theme } from '@emotion/react';
+// import isPropValid from '@emotion/is-prop-valid';
+// import { css } from '@emotion/react';
+const variantMapping = {
+  h1: 'h1',
+  h2: 'h2',
+  h3: 'h3',
+  h4: 'h4',
+  h5: 'h5',
+  h6: 'h6',
+  title1: 'h3',
+  title2: 'h4',
+  title3: 'h5',
+  body1: 'p',
+  body2: 'p',
+  body3: 'p',
+  label1: 'p',
+  label2: 'p',
+  label3: 'p',
+  link1: 'a',
+  link2: 'a',
+  link3: 'a',
+} as const;
 
-/** @description: Workshop Topics:
- *  1. Importance of DS in engineering:
- *     - Challenges that I've experienced in building a MF Arch. with > 1 DS
- *     - Starting projects without a DS
- *  2. Common UI Patterns in Component Libraries and Applying Tokens:
- *     - Child Pass through
- *     - Forward Ref
- *     - Props spreading
- *        a.) ComponentPropsWithoutRef
- *        b.) ComponentPropsWithRef
- *     - Opinionated default
- *  3. How to contribute in Equity-UI repo:
- *     - PR and Code Review Process
- *     - SemVer
- *     - Publishing to NPM
- *
- */
+type TypographyVariant = keyof typeof variantMapping;
+// type AsProps = (typeof variantMapping)[TypographyVariant];
 
-type TypographyVariant =
-  | 'h1'
-  | 'h2'
-  | 'h3'
-  | 'h4'
-  | 'h5'
-  | 'h6'
-  | 'titlel'
-  | 'title2'
-  | 'title3'
-  | 'body1'
-  | 'body2'
-  | 'body3'
-  | 'label1'
-  | 'label2'
-  | 'label3'
-  | 'link1'
-  | 'link2'
-  | 'link3';
+// type TypographyVariant =
+//   | 'h1'
+//   | 'h2'
+//   | 'h3'
+//   | 'h4'
+//   | 'h5'
+//   | 'h6'
+//   | 'title1'
+//   | 'title2'
+//   | 'title3'
+//   | 'body1'
+//   | 'body2'
+//   | 'body3'
+//   | 'label1'
+//   | 'label2'
+//   | 'label3'
+//   | 'link1'
+//   | 'link2'
+//   | 'link3';
 
-type ComponentVariants = 'div' | 'span' | 'a';
+// type ComponentVariants = 'div' | 'span' | 'a';
 
-export type TypographyProps = ComponentPropsWithoutRef<'div'> & {
+export type TypographyProps = ComponentPropsWithoutRef<'button'> & {
   variant?: TypographyVariant;
-  component?: ComponentVariants;
-  color?: string;
+  theme?: Theme;
+  color?: CSSProperties['color'];
   css?: CSSObject;
-  children?: ReactNode;
+  children?: string;
 };
 
-const Typography: React.FC<TypographyProps> = ({
-  variant = 'body2',
+const StyledTypography = styled.div<TypographyProps>((props) => {
+  const { variant, theme, color: colorOverride } = props;
+  const { typography } = theme;
+  const color = colorOverride ?? theme.semantic.color.content.default;
+  const linkColor = theme.semantic.color.content.link;
+
+  switch (variant) {
+    case 'h1':
+      return { ...typography.heading.h1, color };
+    case 'h2':
+      return { ...typography.heading.h2, color };
+    case 'h3':
+      return { ...typography.heading.h3, color };
+    case 'h4':
+      return { ...typography.heading.h4, color };
+    case 'h5':
+      return { ...typography.heading.h5, color };
+    case 'h6':
+      return { ...typography.heading.h6, color };
+    case 'body1':
+      return { ...typography.body.large, color };
+    case 'body3':
+      return { ...typography.body.small, color };
+    case 'title1':
+      return { ...typography.title.large, color };
+    case 'title2':
+      return { ...typography.title.default, color };
+    case 'title3':
+      return { ...typography.title.small, color };
+    case 'label1':
+      return { ...typography.label.large, color };
+    case 'label2':
+      return { ...typography.label.default, color };
+    case 'label3':
+      return { ...typography.label.small, color };
+    case 'link1':
+      return { ...typography.link.large, color: linkColor };
+    case 'link2':
+      return { ...typography.link.default, color: linkColor };
+    case 'link3':
+      return {
+        ...typography.link.small,
+        color: linkColor,
+      };
+    case 'body2':
+    default:
+      return { ...typography.body.default, color };
+  }
+});
+
+const Typography = ({
   children,
   color,
-  component: Component = 'div',
-  css: cssOveride,
+  variant = 'body2',
+  css,
   ...props
-}) => {
-  const TypographyRoot = styled.div`
-    ${({ theme }) => css`
-      font-family: ${theme.typography.body.default.fontFamily};
-      color: ${color ? color : theme.semantic.color.content.default};
-      letter-spacing: ${theme.typography.body.default.letterSpacing}px;
-      text-align: left;
-      font-weight: ${theme.typography.body.default.fontWeight};
-    `}
-
-    ${() => {
-      switch (variant) {
-        case 'h1':
-          return ({ theme }) => css`
-            font-size: ${theme.typography.heading.h1.fontSize}px;
-            line-height: ${theme.typography.heading.h1.lineHeight}px;
-          `;
-        case 'h2':
-          return ({ theme }) => css`
-            font-size: ${theme.typography.heading.h2.fontSize}px;
-            line-height: ${theme.typography.heading.h2.lineHeight}px;
-          `;
-        case 'h3':
-          return ({ theme }) => css`
-            font-size: ${theme.typography.heading.h3.fontSize}px;
-            line-height: ${theme.typography.heading.h3.lineHeight}px;
-          `;
-        case 'h4':
-          return css`
-            font-size: 21px;
-            font-weight: 600;
-            line-height: 28px;
-          `;
-        case 'h5':
-          return css`
-            font-size: 16px;
-            font-weight: 600;
-            line-height: 22px;
-          `;
-        case 'h6':
-          return css`
-            font-size: 14px;
-            font-weight: 600;
-            line-height: 18px;
-          `;
-        case 'titlel':
-          return css`
-            font-size: 24px;
-            line-height: 32px;
-          `;
-        case 'title2':
-          return css`
-            font-size: 20px;
-            line-height: 22px;
-          `;
-        case 'title3':
-          return css`
-            font-size: 18px;
-            line-height: 22px;
-          `;
-        case 'label1':
-          return css`
-            font-size: 20px;
-            font-weight: 600;
-            line-height: 24px;
-          `;
-        case 'label2':
-          return css`
-            font-size: 16px;
-            font-weight: 600;
-            line-height: 22px;
-          `;
-        case 'label3':
-          return css`
-            font-size: 14px;
-            font-weight: 600;
-            line-height: 18px;
-          `;
-        case 'link1':
-          return css`
-            font-size: 20px;
-            font-weight: 600;
-            line-height: 24px;
-            color: ${color};
-          `;
-        case 'link2':
-          return ({ theme }) => css`
-            font-size: ${theme.typography.link.default.fontSize}px;
-            font-weight: ${theme.typography.link.default.fontWeight};
-            line-height: ${theme.typography.link.default.lineHeight}px;
-            color: ${color ?? theme.semantic.color.content.link};
-            &:hover {
-              color: ${theme.semantic.color.content.linkHover};
-            }
-          `;
-        case 'link3':
-          return css`
-            font-size: 16px;
-            line-height: 20px;
-            color: ${color};
-          `;
-        case 'body1':
-          return css`
-            font-size: 20px;
-            font-weight: 400;
-            line-height: 28px;
-          `;
-        case 'body3':
-          return css`
-            font-size: 14px;
-            font-weight: 400;
-            line-height: 22px;
-          `;
-        case 'body2':
-        default:
-          return css`
-            font-size: 16px;
-            font-weight: 400;
-            line-height: 28px;
-          `;
-      }
-    }}
-  `;
-
+}: TypographyProps) => {
   return (
-    <TypographyRoot color={color} css={cssOveride} as={Component} {...props}>
+    <StyledTypography
+      color={color}
+      variant={variant}
+      css={css}
+      as={variantMapping[variant]}
+      {...props}
+    >
       {children}
-    </TypographyRoot>
+    </StyledTypography>
   );
 };
 
