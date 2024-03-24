44b61d7bc91a8fb82f164839c4d58b486227377e
Carlo <ca.fernando415@gmail.com>
2024-03-23 23:23:52 -0400

    fixed states related to link variants

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as contextRenderer } from './contextRender';
 export { default as transformValueToUnit } from './transformValueToUnit';
+export { default as linkVariantAdditionalState } from './linkVariantAdditionalState';
