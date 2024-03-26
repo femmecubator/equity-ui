05b95219fd5ccf3a37a8ee7f3d98e235f667638b
Aminat Adebayo <adebayo.aminat@gmail.com>
2024-03-26 11:08:06 -0400

    adding tab component

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,4 +1,29 @@
-import React from 'react';
+// import React, { MouseEventHandler } from 'react';
+// // import styled from '@emotion/styled';
+
+// export type TabProps = {
+//   children: React.ReactNode;
+//   variant?: 'default' | 'active';
+//   disabled?: boolean;
+//   onClick?: MouseEventHandler; //blue underline?
+// }
+
+//variables to define the defualt tab and active tab
+// Define variants
+// const tabVariant = {
+//   default: {
+//     label: 'montserrat-regular-16',
+//     color: 'color-content-default',
+//   },
+//   active: {
+//     label: 'montserrat-semibold-16',
+//     color: 'color-content-active',
+//   },
+// };
+
+// const StyledTab = styled.div<TabProps>`
+// //adding styling here
+// `
 
 function Tab() {
   return <div>Tab</div>;
@@ -8,4 +33,11 @@ export default Tab;
 
 /*
 create two tabs one Default another Active
+
+the component should have a label
+tab groups should have set amount of tab
+active tabs should have the horizontal blue line according to the figma specs
+tab component should follow all the style specs defined in figma
+should have unit test that would test if the component is mounting successfully, test for the label, # of tabs within the tab group, active status, default status
++should have a storybook
 */
