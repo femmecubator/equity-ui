27780518b6525ea26c61d2c36a79fb9d2b4ad1f0
Clae <56447997+clayruh@users.noreply.github.com>
2024-03-27 14:42:00 -0400

    Clean up code and comments. Add additional test for severity

--- src/components/Badge/Badge.test.tsx
+++ src/components/Badge/Badge.test.tsx
@@ -4,13 +4,26 @@ import { Badge } from '.';
 import contextRender from '../../shared/test/contextRender';
 
 describe('Badge component', () => {
-    // it or test ('title of the test')
-    // can be nested inside describe or not
-    test('Badge should render correctly', () => {
-        contextRender(<Badge severity={'error'} isStrong={false}>Error</Badge>);
-        const badge = screen.getByText('Error');
-        expect(badge).toBeInTheDocument();
-    })
-} )
+  // it or test ('title of the test')
+  // can be nested inside describe or not
+  test('Badge should render correctly', () => {
+    contextRender(
+      <Badge severity={'error'} isStrong={false}>
+        Error
+      </Badge>
+    );
+    const badge = screen.getByText('Error');
+    expect(badge).toBeInTheDocument();
+  });
 
-// maybe need another test for 'strong' CSS change
+  test('Badge isStrong should change', () => {
+    contextRender(
+      <Badge severity={'error'} isStrong={true}>
+        Error
+      </Badge>
+    );
+    const badge = screen.getByText('Error');
+    expect(badge).toBeInTheDocument();
+    screen.debug(badge);
+  });
+});
--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -8,14 +8,14 @@ const SEVERITY_TO_BG_COLOR_MAPPING = {
   success: base.color.green50,
   warning: base.color.yellow50,
   info: base.color.sky50,
-} as const
+} as const;
 
 const SEVERITY_TO_SUBTLE_BG_COLOR_MAPPING = {
   error: base.color.red15,
   success: base.color.green15,
   warning: base.color.yellow20,
   info: base.color.sky15,
-} as const
+} as const;
 
 const StyledBadge = styled.div<BadgeProps>`
   display: inline-block;
@@ -29,31 +29,42 @@ const StyledBadge = styled.div<BadgeProps>`
     severity,
     isStrong,
     theme: {
-      semantic: { border }
+      semantic: { border },
     },
   }) =>
-  // all the following code comes from semantic.ts file because we said theme: { semantic: {base} }
-  // everything below here is meant to be dynamic
-  `
+    // all the following code comes from semantic.ts file because we said theme: { semantic: {base} }
+    // everything below here is meant to be dynamic
+    `
     border-radius: ${border.radius.small};
-    background-color: ${isStrong? SEVERITY_TO_BG_COLOR_MAPPING[severity] : SEVERITY_TO_SUBTLE_BG_COLOR_MAPPING[severity]};
-    color: ${ severity === 'warning'? base.color.gray90 : isStrong? base.color.white : base.color.gray90};
+    background-color: ${
+      isStrong
+        ? SEVERITY_TO_BG_COLOR_MAPPING[severity]
+        : SEVERITY_TO_SUBTLE_BG_COLOR_MAPPING[severity]
+    };
+    color: ${
+      severity === 'warning'
+        ? base.color.gray90
+        : isStrong
+          ? base.color.white
+          : base.color.gray90
+    };
   `}
 `;
 export type BadgeProps = {
-  children: ReactNode
-  // severity is a prop we're taking
-  // making severity optional so that the default is success
+  children: ReactNode;
   severity: 'error' | 'success' | 'warning' | 'info';
-  isStrong: boolean
-}
+  isStrong: boolean;
+};
 
 // destructuring props to the specifics, and defining a default for severity, emphasis, etc
-export default function Badge({severity = 'error', isStrong = false, children}: BadgeProps) {
-  function registerClick() {
-    console.log('clicked');
-  }
-
-  // adding in severity here, is how it'll access from the css section
-  return <StyledBadge severity={severity} isStrong={isStrong} onClick={registerClick}>{children}</StyledBadge>;
+export default function Badge({
+  severity = 'error',
+  isStrong = false,
+  children,
+}: BadgeProps) {
+  return (
+    <StyledBadge severity={severity} isStrong={isStrong}>
+      {children}
+    </StyledBadge>
+  );
 }
