a2d6524662191aa029f185d48e3d3818c849d05a
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2024-03-27 12:14:48 -0400

    destructured the necessary parts of the theme object within the styled component

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -6,40 +6,48 @@ import { EquityTheme } from '../../theme';
 
 //Adjust StyledButton to accept disabled as a prop and apply styles conditionally
 const StyledButton = styled.button<{ disabled?: boolean }>`
-  ${({ theme, disabled }) => `
-  background-color: transparent; /* Set background to transparent */
-  cursor: pointer;
-  border: none;
-  display: flex;
-  width: 100%; /* Adjusted to 100% */
-  padding: ${theme.base.spacing.spacing4};
-  align-items: flex-start;
-  gap: ${theme.base.spacing.spacing8};
-  border-radius: ${theme.base.border.radius.radius4};
-  color: ${disabled ? theme.base.color.gray60 : theme.base.color.gray90};
-  /* body/small */
-  font-family: Montserrat;
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 22px; /* 157.143% */
+  ${({ theme, disabled }) => {
+    //Destructuring theme properties for easier access
+    const { spacing, border, color } = theme.base;
+    const { spacing4, spacing8 } = spacing;
+    const { radius4 } = border.radius;
+    const { gray60, gray90, gray30, blue50 } = color;
 
-  /* Hover state */
-  &:hover {
-    background-color: transparent; /* Set background to transparent */
-    color: ${disabled ? theme.base.color.gray30 : theme.base.color.blue50};
-  }
-
-  /* Focus state */
-  &:focus {
-    color: ${disabled ? theme.base.color.gray30 : theme.base.color.gray90};
-    outline: ${disabled ? 'none' : `1px solid ${theme.base.color.gray90}`};
-  }
-
-  /* Disabled state */
-  &:disabled {
-    cursor: not-allowed;
-  }
-`}
+    return `
+      background-color: transparent; /* Set background to transparent */
+      cursor: pointer;
+      border: none;
+      display: flex;
+      width: 100%; /* Adjusted to 100% */
+      padding: ${spacing4};
+      align-items: flex-start;
+      gap: ${spacing8};
+      border-radius: ${radius4};
+      color: ${disabled ? gray60 : gray90};
+      /* body/small */
+      font-family: Montserrat;
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 22px; /* 157.143% */
+      
+      /* Hover state */
+      &:hover {
+        background-color: transparent; /* Set background to transparent */
+        color: ${disabled ? gray30 : blue50};
+      }
+      
+      /* Focus state */
+      &:focus {
+        color: ${disabled ? gray30 : gray90};
+        outline: ${disabled ? 'none' : `1px solid ${gray90}`};
+      }
+      
+      /* Disabled state */
+      &:disabled {
+        cursor: not-allowed;
+      }
+    `;
+  }}
 `;
 
 export interface MenuItemProps
