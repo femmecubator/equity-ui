4422afbb3135152d4d96d9bbfe5dec6ef19f51c8
Carlo <ca.fernando415@gmail.com>
2024-03-31 15:42:10 -0400

    solution for tab component

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import { ComponentPropsWithoutRef, ReactElement } from 'react';
 import styled from '@emotion/styled';
 
-export type TabProps = {
-  children: () => React.ReactNode;
-  isActive?: boolean;
-};
+export interface TabItemProps
+  extends Omit<ComponentPropsWithoutRef<'div'>, 'onChange'> {
+  isActive: boolean;
+  tabItemId: number;
+  label: string;
+}
 
-const StyledTabItem = styled.div<TabProps>`
+const StyledTabItem = styled.div<Omit<TabItemProps, 'label'>>`
   ${({ theme, isActive }) => `
     display: inline-flex;
     text-align: center;
@@ -17,25 +20,57 @@ const StyledTabItem = styled.div<TabProps>`
     align-items: center;
     width: 125px;
     height: 44px;
-    padding: 12px;
+    padding-bottom: 9px;
+    padding-top: 9px;
+    padding-left: 16px;
+    padding-right: 16px;
     gap: 10px;
     font-weight: ${
-      isActive === true
-        ? '600'
-        : `${theme.typography.body.default.fontWeight}`
+      isActive === true ? '600' : `${theme.typography.body.default.fontWeight}`
     };
     line-height: ${
-      isActive === true
-        ? '22px'
-        : `${theme.typography.body.default.lineHeight}`
+      isActive === true ? '22px' : `${theme.typography.body.default.lineHeight}`
     }; 
     border-bottom: ${isActive == true ? '2px solid #026FE4' : undefined}
 `}
 `;
 
-function TabGroup({ isActive = false }) {
-  return <StyledTabItem isActive={isActive}>tab</StyledTabItem>;
+export const TabItem: React.FC<TabItemProps> = ({
+  label,
+  tabItemId,
+  ...props
+}) => {
+  return (
+    <StyledTabItem {...props} tabItemId={tabItemId}>
+      {label}
+    </StyledTabItem>
+  );
+};
+
+export interface TabGroupProps
+  extends Omit<ComponentPropsWithoutRef<'div'>, 'onChange' | 'children'> {
+  tabItems: TabItemProps[];
+  onChange: (activeKey: number) => void;
+  children: (props: {
+    tabItems: TabItemProps[];
+    onChange: (activeKey: number) => void;
+  }) => ReactElement[];
 }
+
+function TabGroupRoot({ children, className, ...props }: TabGroupProps) {
+  return (
+    <div>
+      <div>{children(props)}</div>
+      <div className={className}>
+        {props.tabItems.find((item) => item.isActive)?.children}
+      </div>
+    </div>
+  );
+}
+
+const TabGroup = styled(TabGroupRoot)`
+  padding-top: 15px;
+`;
 
 export default TabGroup;
 
--- src/components/Tab/__docs__/Tab.stories.tsx
+++ src/components/Tab/__docs__/Tab.stories.tsx
@@ -1,17 +1,71 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import type { Meta } from '@storybook/react';
-import TabGroup from '../Tab';
+import TabGroup, { TabGroupProps, TabItem, TabItemProps } from '../Tab';
 
-const meta: Meta<typeof TabGroup> = {
+const meta: Meta<TabGroupProps> = {
   title: 'TabGroup',
   component: TabGroup,
-  argTypes: {
-    isActive: {
-      control: { type: 'boolean' },
-    },
-  },
 };
 
 export default meta;
 
-export const Basic = (args) => <TabGroup {...args}></TabGroup>;
+const itemsInitial: TabItemProps[] = [
+  {
+    isActive: true,
+    label: 'one',
+    children: <div>test one</div>,
+    tabItemId: 1,
+  },
+  {
+    isActive: false,
+    label: 'two',
+    children: <div>test two</div>,
+    tabItemId: 2,
+  },
+  {
+    isActive: false,
+    label: 'three',
+    children: <div>test 3</div>,
+    tabItemId: 3,
+  },
+];
+
+const Template = () => {
+  const [items, setItems] = useState<TabItemProps[]>(itemsInitial);
+
+  const handleChange = useCallback((activeKey: number) => {
+    setItems((prev) => {
+      prev.forEach((tabItem) => {
+        if (tabItem.tabItemId === activeKey) {
+          tabItem.isActive = true;
+        }
+
+        if (tabItem.isActive && tabItem.tabItemId !== activeKey) {
+          tabItem.isActive = false;
+        }
+      });
+      return [...prev];
+    });
+  }, []);
+
+  return (
+    <TabGroup tabItems={items} onChange={handleChange}>
+      {({ onChange, tabItems }) =>
+        tabItems.map(({ isActive, tabItemId, label, ...props }) => {
+          return (
+            <TabItem
+              {...props}
+              key={tabItemId}
+              isActive={isActive}
+              tabItemId={tabItemId}
+              onClick={() => onChange(tabItemId)}
+              label={label}
+            />
+          );
+        })
+      }
+    </TabGroup>
+  );
+};
+
+export const Tabs = Template.bind({});
