e3cbdfe1868434c6cbee8b659c59dd4f4f185a1a
Aminat Adebayo <adebayo.aminat@gmail.com>
2024-04-01 19:26:12 -0400

    tab solutions

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import { ComponentPropsWithoutRef, ReactElement } from 'react';
 import styled from '@emotion/styled';
 
-export type TabProps = {
-  children: () => React.ReactNode;
-  isActive?: boolean;
-};
+export interface TabItemProps
+  extends Omit<ComponentPropsWithoutRef<'div'>, 'onChange'> {
+  isActive: boolean;
+  tabItemId: number;
+  label: string;
+}
 
-const StyledTabItem = styled.div<TabProps>`
+const StyledTabItem = styled.div<Omit<TabItemProps, 'label'>>`
   ${({ theme, isActive }) => `
     display: inline-flex;
     text-align: center;
@@ -17,35 +20,56 @@ const StyledTabItem = styled.div<TabProps>`
     align-items: center;
     width: 125px;
     height: 44px;
-    padding: 12px;
+    padding-bottom: 9px;
+    padding-top: 9px;
+    padding-left: 16px;
+    padding-right: 16px;
     gap: 10px;
     font-weight: ${
-      isActive === true
-        ? '600'
-        : `${theme.typography.body.default.fontWeight}`
+      isActive === true ? '600' : `${theme.typography.body.default.fontWeight}`
     };
     line-height: ${
-      isActive === true
-        ? '22px'
-        : `${theme.typography.body.default.lineHeight}`
+      isActive === true ? '22px' : `${theme.typography.body.default.lineHeight}`
     }; 
     border-bottom: ${isActive == true ? '2px solid #026FE4' : undefined}
 `}
 `;
 
-function TabGroup({ isActive = false }) {
-  return <StyledTabItem isActive={isActive}>tab</StyledTabItem>;
+export const TabItem: React.FC<TabItemProps> = ({
+  label,
+  tabItemId,
+  ...props
+}) => {
+  return (
+    <StyledTabItem {...props} tabItemId={tabItemId}>
+      {label}
+    </StyledTabItem>
+  );
+};
+
+export interface TabGroupProps
+  extends Omit<ComponentPropsWithoutRef<'div'>, 'onChange' | 'children'> {
+  tabItems: TabItemProps[];
+  onChange: (activeKey: number) => void;
+  children: (props: {
+    tabItems: TabItemProps[];
+    onChange: (activeKey: number) => void;
+  }) => ReactElement[];
 }
 
+function TabGroupRoot({ children, className, ...props }: TabGroupProps) {
+  return (
+    <div>
+      <div>{children(props)}</div>
+      <div className={className}>
+        {props.tabItems.find((item) => item.isActive)?.children}
+      </div>
+    </div>
+  );
+}
+
+const TabGroup = styled(TabGroupRoot)`
+  padding-top: 15px;
+`;
+
 export default TabGroup;
-
-/*
-+the component should have a label
-tab groups should have set amount of tab
-+active tabs should have the horizontal blue line according to the figma specs
-+tab component should follow all the style specs defined in figma
-should have unit test that would test if the component is mounting successfully, test for the label, # of tabs within the tab group, active status, default status
-+should have a storybook
-
-tab can be clicked 
-*/
