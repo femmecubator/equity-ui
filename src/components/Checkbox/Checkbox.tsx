48aca7ba0d45f0f42ffae12100b5295df82b1dd4
Netaly <25016486+21shield@users.noreply.github.com>
2025-01-10 22:47:54 -0500

    Creates tests for Checkbox and Label Checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 import checkmark from './checkmark.svg';
 import Label from '../Label';
-import { CheckboxGroup } from './CheckboxGroup';
 
 const StyledCheckbox = styled.input<CheckboxProps>`
   -moz-appearance: none;
@@ -15,18 +14,19 @@ const StyledCheckbox = styled.input<CheckboxProps>`
       justifyContent: 'center',
       height: '22px',
       width: '22px',
-      border: `2px solid ${theme.semantic.color.border.default}`,
+      border: '2px solid',
+      borderColor: theme.semantic.color.border.default,
       borderRadius: theme.semantic.border.radius.small,
 
       ...(error
         ? {
-            border: `2px solid ${theme.semantic.color.border.error}`,
+            borderColor: theme.semantic.color.border.error,
           }
         : undefined),
 
       '&:checked': {
         background: theme.semantic.color.bg.brand,
-        border: `2px solid ${theme.semantic.color.border.default}`,
+        borderColor: theme.semantic.color.border.default,
         '&:after': {
           content: "''",
           background: 'white',
@@ -39,10 +39,11 @@ const StyledCheckbox = styled.input<CheckboxProps>`
       },
 
       '&:focus': {
-        border: `2px solid ${theme.semantic.color.border.active}`,
+        borderColor: theme.semantic.color.border.active,
       },
 
       '&:disabled': {
+        borderColor: theme.semantic.color.border.default,
         background: theme.semantic.color.bg.disabled,
       },
     };
@@ -51,26 +52,22 @@ const StyledCheckbox = styled.input<CheckboxProps>`
 
 export type CheckboxProps = Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
-  'type'
+  'type' | 'role'
 > & { error?: boolean; label?: string };
 
 export const Checkbox = (props: CheckboxProps) => {
   if (!props.label) {
-    return <StyledCheckbox type="checkbox" {...props} />;
+    return <StyledCheckbox role="checkbox" type="checkbox" {...props} />;
   }
   return (
-    <CheckboxGroup>
-      <StyledCheckbox type="checkbox" {...props} />
-
-      <Label
-        htmlFor={props.id}
-        error={props.error}
-        disabled={props.disabled}
-        checked={props.disabled}
-      >
-        {props.label}
-      </Label>
-    </CheckboxGroup>
+    <Label
+      error={props.error}
+      disabled={props.disabled}
+      checked={props.disabled}
+    >
+      <StyledCheckbox role="checkbox" type="checkbox" {...props} />
+      {props.label}
+    </Label>
   );
 };
 
